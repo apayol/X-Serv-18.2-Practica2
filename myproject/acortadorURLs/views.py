6cade9a9bad21ace59203907d03afef5b8522ef4
apayol <apayolmontero@gmail.com>
2018-04-10 18:02:46 +0200

    Método GET de views

--- myproject/acortadorURLs/views.py
+++ myproject/acortadorURLs/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Urls
 
 # Create your views here.
 
 def bienvenida(request):
-    return HttpResponse("<h3>Bienvenido a acortadorURLs<h3>")
+    if request.method == "GET":
+        respuesta = "<h3>Bienvenido a acortadorURLs</h3>"
+        respuesta += "<br>URLs guardadas:"
+        urls = Urls.objects.all()
+        for url in urls:
+            respuesta += "<br>Url original: " + url.name + " => Url corta: " + str(url.id)
+    return HttpResponse(respuesta)
 
 def redir(request, url_corta):
-    return HttpResponse("<h3>Redirección<h3>")
+    return HttpResponse("<h3>Redirección</h3>")
