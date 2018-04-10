d3c9dcc29862d3ed2b642636a3f3474b6705c831
apayol <apayolmontero@gmail.com>
2018-04-10 18:12:44 +0200

    Añadido formulario a página de bienvenida views.py

--- myproject/acortadorURLs/views.py
+++ myproject/acortadorURLs/views.py
@@ -4,6 +4,14 @@ from .models import Urls
 
 # Create your views here.
 
+FORMULARIO = """
+    <form action="" method="POST">
+        Introduzca URL para reducir:
+        <input type="text" name="URL" value="http://">
+        <input type="submit" value="Enviar">
+    </form>
+"""
+
 def bienvenida(request):
     if request.method == "GET":
         respuesta = "<h3>Bienvenido a acortadorURLs</h3>"
@@ -11,6 +19,8 @@ def bienvenida(request):
         urls = Urls.objects.all()
         for url in urls:
             respuesta += "<br>Url original: " + url.name + " => Url corta: " + str(url.id)
+        respuesta += "<br><br>" + FORMULARIO
+
     return HttpResponse(respuesta)
 
 def redir(request, url_corta):
